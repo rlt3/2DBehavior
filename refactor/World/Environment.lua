ae75f18f0565c307cdfbfb12132043db198eae3a
rlt3 <rlt3@utk.edu>
2023-01-26 13:54:50 -0500

    Adding the base for Entity & Environment, as well as a starting Template for Entities.

---
+++ refactor/World/Environment.lua
@@ -0,0 +1,24 @@
+local Environment = {}
+Environment.__index = Environment
+
+function Environment:init ()
+    self.Entities = {}
+end
+
+function Environment:add (entity)
+    table.insert(self.Entities, entity)
+end
+
+function Environment:draw (Viewport)
+    for i,entity in ipairs(self.Entities) do
+        entity:draw(Viewport)
+    end
+end
+
+function Environment:update (dt)
+    for i,entity in ipairs(self.Entities) do
+        entity:update(dt)
+    end
+end
+
+return Environment
--- refactor/World/World.lua
+++ refactor/World/World.lua
@@ -1,12 +1,15 @@
 local Map = require("World/Map")
+local Environment = require("World/Environment")
 
 local World = {}
 World.__index = World
 
 function World:init ()
     self.Map = Map
+    self.Environment = Environment
 
     Map:init()
+    Environment:init()
 end
 
 function World:pause ()
@@ -20,6 +23,7 @@ end
 
 function World:draw (Viewport)
     Map:draw(Viewport)
+    Environment:draw(Viewport)
 end
 
 return setmetatable(World, World)
